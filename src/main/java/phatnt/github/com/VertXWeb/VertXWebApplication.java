698aae4709df213883e4afcd01ebe7a878d9e638
phatnt <phatnt@vihatgroup.com>
2021-11-20 08:47:33 +0700

    init source

---
+++ src/main/java/phatnt/github/com/VertXWeb/VertXWebApplication.java
@@ -0,0 +1,13 @@
+package phatnt.github.com.VertXWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertXWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertXWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phatnt/github/com/VertXWeb/VertXWebApplicationTests.java
@@ -0,0 +1,13 @@
+package phatnt.github.com.VertXWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertXWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
